f6f7d06883e7083dedcebcf3bfcc002a50944f3b
Taha Özkan <129782875+taha0zkan@users.noreply.github.com>
2024-12-29 22:02:21 +0300

    Added css file
    I added css file

--- Users/Manager.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-
-public class Manager extends Employee 
-{
-    public Manager(int ID, String Username,String Role,String Name,String Surname,String Phone,LocalDate DOB,LocalDate DOS,String Email,String Password, Boolean DEFAULT_PASSWORD)
-    {
-        super(ID,  Username, Role, Name, Surname, Phone, DOB, DOS, Email, Password);
-    }
-}
