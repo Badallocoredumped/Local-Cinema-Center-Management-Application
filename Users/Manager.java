ccbf651ac24d3490994596eb1e543595c5dcbf44
Maryam Khan <99869423+MaryamKhan23@users.noreply.github.com>
2025-01-04 14:01:46 +0300

    Update Manager.java

--- Users/Manager.java
+++ Users/Manager.java
@@ -2,11 +2,14 @@ import java.lang.reflect.InvocationHandler;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.Scanner;
+<<<<<<< Updated upstream
 
 import utilities.DataBaseHandler;
 import utilities.InputHandler;
 import utilities.InputUtil;
 
+=======
+>>>>>>> Stashed changes
 
 public class Manager extends Employee 
 {
@@ -19,8 +22,9 @@ public class Manager extends Employee
         super(ID,  Username, Role, Name, Surname, Phone, DOB, DOS, Email, Password);
     }
 
-    private static Scanner scanner = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
 
+<<<<<<< Updated upstream
     @Override
     public void Menu(){
         Scanner scanner = InputUtil.scanner;
@@ -29,11 +33,22 @@ public class Manager extends Employee
         while (MInput != '5'){
             System.out.println("Welcome to Manager Menu");
             //System.out.println("Good day, " getRole()+ " " + getName()+ " " +getSurname()+ "!");
+=======
+    
+    public void ManagerMenu(){
+
+        String menuString;
+        char menuChoice = '0'; 
+
+        while (menuChoice!= '5'){
+            System.out.println("Welcome to Manager Menu");
+>>>>>>> Stashed changes
             System.out.println("1. View and Update Inventory");
             System.out.println("2. Manage Personnel Records");
             System.out.println("3. Update Prices and Discounts");
             System.out.println("4. View Revenue and Taxes");
             System.out.println("5. Logout");
+<<<<<<< Updated upstream
             System.out.println("What would you like to select. Pick from option 1-5: ");
 
             MIString = scanner.nextLine();
@@ -64,6 +79,36 @@ public class Manager extends Employee
                     clearConsole();
                     System.out.println("Logging you out.");
                     return;
+=======
+            System.out.println("Please select an option: ");
+
+            menuString = input.nextLine();
+
+            switch(menuChoice){
+                case '1' -> {
+                    Ccleaner();
+                    manageInventory();
+                }  
+                case '2' -> {
+                    Ccleaner();
+                    managePersonnel();
+                }
+                case '3' -> {
+                    Ccleaner();
+                    updatePriceandDiscount();
+                }
+                case '4' -> {
+                    Ccleaner();
+                    viewRevenueandTax();
+                }
+                case '5' -> {
+                    Ccleaner();
+                    System.out.println("Logging you out.");
+                }
+                default -> {
+                    System.out.println("Invalid choice. Re-enter choice:");
+                }
+>>>>>>> Stashed changes
             }
         }
         scanner.close();
@@ -92,6 +137,7 @@ public class Manager extends Employee
         }
     }
 
+<<<<<<< Updated upstream
     protected void hireEmployees() {
         String name = inHandle.getStringInput("Enter Employee's name: ");
         String surname = inHandle.getStringInput("Enter Employee's surname: ");
@@ -119,15 +165,116 @@ public class Manager extends Employee
             System.out.println("Employee fired successfully!");
         } else {
             System.out.println("Employee not found.");
+=======
+    private void manageInventory(){
+        System.out.println("View and Update Inventory: ");
+        System.out.println("1. Bevergaes - Stock: 100");
+        System.out.println("2. Biscuits - Stock: 200");
+        System.out.println("3. Toys - Stcok: 50");
+        System.out.println("Select a product to update stock from 1-3, or 0 to return: ");
+        
+        String choice = input.nextLine();
+        if(choice.equals("0")) return; 
+        try {
+            int productChoice = Integer.parseInt(choice);
+            if (productChoice >= 1 && productChoice <= 3){
+                System.out.print("Enter quantity to add to stock: ");
+                int additionalStock = Integer.parseInt(input.nextLine());
+                System.out.print("Stock update successfully.");
+            } else {
+                System.out.println("Invalid product choice.");
+            }
+        
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+        }
+    }
+
+    private void managePersonnel(){
+        System.out.println("View and Edit Personnel Records:");
+        System.out.println("1. John Doe - Role: Cashier");
+        System.out.println("2. Jane Smith - Role: Manager");
+        System.out.println("Select a personal record to edit or 0 to return: ");
+
+        String choice = input.nextLine(); 
+        if(choice.equals("0")) return;
+        
+        try {
+            int personnelChoice = Integer.parseInt(choice);
+            if(personnelChoice ==1 || personnelChoice == 2){
+                System.out.println("Editing details for selected personnel.");
+                System.out.println("Enter new first name: ");
+                String firstName = input.nextLine();
+                System.out.println("Enter new last name: ");
+                String LastName = input.nextLine();
+                System.out.println("Enter new username: ");
+                String username = input.nextLine();
+                System.out.println("Enter new password:");
+                String password  = input.nextLine();
+                System.out.println("Enter new role");
+                String role = input.nextLine();
+                System.out.println("personnel record updated successfully.");
+            } else {
+                System.out.println("Invalid personnel choice.");
+            }
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+>>>>>>> Stashed changes
         }
     }
 
 
+<<<<<<< Updated upstream
 
     protected void updatePriceandDiscount(){
         System.out.println("Welcome to System!");
     }
     protected void viewRevenueandTax(){
         System.out.println("Welcome to System!");
+=======
+    private void updatePriceandDiscount(){
+        System.out.println("Update Prices and Discounts:");
+        System.out.println("1. Cinema Ticket Price");
+        System.out.println("2. Product Prices");
+        System.out.println("3. Age-Based Discount Rate");
+        System.out.println("Select an option to update or 0 to return: ");
+
+        String choice = input.nextLine(); 
+        if(choice.equals("0")) return;
+        
+        try {
+            int updateChoice = Integer.parseInt(choice);
+            switch(updateChoice){
+                case 1 -> {
+                    System.out.println("Enter new cinema ticket price: ");
+                    double ticketPrice = Double.parseDouble(input.nextLine());
+                    System.out.println("Cinema ticket price updated successfully.");
+                }
+                case 2 -> {
+                    System.out.println("Enter product ID to update price: ");
+                    double productID = Double.parseDouble(input.nextLine());
+                    System.out.println("Enter new price to update: ");
+                    double productPrice = Double.parseDouble(input.nextLine());
+                    System.out.println("Product price updated successfully.");
+                }
+                case 3 -> {
+                    System.out.println("Enter new age-based discount rate(hint: 0.2 for 20%): ");
+                    double discountRate = Double.parseDouble(input.nextLine());
+                    System.out.println("Discount rate updated successfully.");
+                }
+                default -> System.out.println("nvalid choice.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+        }        
+    }
+
+    private void viewRevenueandTax(){
+        System.out.println("View Revenue and Taxes: "); 
+        //db fetch rev and tax 
+        System.out.println("Total Revenue: "); 
+        System.out.println("Total Tax: "); 
+>>>>>>> Stashed changes
     }
 }
